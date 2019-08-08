b5389d768e76c611658c2361eba7f769894d93c7
vlipatdev <vlipat.dev@gmail.com>
2019-08-08 20:43:30 +0800

    Updates

---
+++ precache-manifest.ce5b3037967e136209c8ac143532c407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cddef289a0047e05a5509c15e556a21",
+    "url": "/frontend-resources/index.html"
+  },
+  {
+    "revision": "0b4c62ee64b1d4af9dee",
+    "url": "/frontend-resources/static/css/main.badb1543.chunk.css"
+  },
+  {
+    "revision": "573b4b6710dd51e2b3df",
+    "url": "/frontend-resources/static/js/2.f3d17cd3.chunk.js"
+  },
+  {
+    "revision": "0b4c62ee64b1d4af9dee",
+    "url": "/frontend-resources/static/js/main.128318f1.chunk.js"
+  },
+  {
+    "revision": "c1d21fe0738a77ee0882",
+    "url": "/frontend-resources/static/js/runtime~main.b33394d5.js"
+  },
+  {
+    "revision": "11cde3a9148b1aeb7bf37cb010b15b38",
+    "url": "/frontend-resources/static/media/undraw-to-the-moon.11cde3a9.png"
+  }
+]);
